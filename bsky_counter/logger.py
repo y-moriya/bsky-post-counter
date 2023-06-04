c4001f854700bbbd04d04399ebb3b636bfc987a6
ryosms <ryo.sms@gmail.com>
2023-06-05 01:08:00 +0900

    Add custom logger

---
+++ bsky_counter/logger.py
@@ -0,0 +1,37 @@
+from logging import config, getLogger, Logger, DEBUG
+
+
+class BskyCounterLogger:
+    _LOGGER_NAME = "BskyCounterLogger"
+    _CONFIG = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {
+                "format": "%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d#%(funcName)s]: %(message)s"
+            }
+        },
+        "handlers": {
+            "consoleHandler": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "simple"
+            }
+        },
+        "loggers": {
+            _LOGGER_NAME: {
+                "level": "WARN",
+                "handlers": ["consoleHandler"]
+            }
+        },
+    }
+    _logger: Logger
+
+    def __init__(self, debug: bool = False):
+        config.dictConfig(self._CONFIG)
+        self._logger = getLogger("BskyCounterLogger")
+        if debug:
+            self._logger.setLevel(DEBUG)
+
+    def get_logger(self) -> Logger:
+        return self._logger
